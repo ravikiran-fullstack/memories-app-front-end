b135221894662e1f9efad8f80a1e212aef530404
kiran-code <ravikiransjce.code@gmail.com>
2021-01-17 15:22:21 +0530

    Added reduer for fetching and posting posts

---
+++ src/reducers/posts.js
@@ -0,0 +1,10 @@
+export const reducer = (posts = [], action) => {
+  switch(action.type){
+    case 'FETCH_ALL_POSTS':
+      return posts;
+    case 'CREATE_POST':
+      return posts;
+    default:
+      return posts;    
+  }
+}
