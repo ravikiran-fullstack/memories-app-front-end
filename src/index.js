f42c0901442966a9ad274593d3d35db351e37833
kiran-code <ravikiransjce.code@gmail.com>
2021-01-18 20:14:24 +0530

    Removed redux devtool string for chrome

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import reducers from './reducers';
 import App from './App';
 import './index.css';
 
-const store = createStore(reducers, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
