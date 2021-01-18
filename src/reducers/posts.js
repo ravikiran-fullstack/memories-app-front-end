9927456c8533f1fc8300b5c37bc2cbfe8ce55820
kiran-code <ravikiransjce.code@gmail.com>
2021-01-18 19:46:07 +0530

    Added action type constants in reducers

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,14 +1,16 @@
+import {CREATE_POST, FETCH_ALL_POSTS, UPDATE_POST, DELETE_POST, LIKE_POST} from '../constants/actionTypes';
+
 export const reducer = (posts = [], action) => {
   switch(action.type){
-    case 'LIKE_POST': 
+    case LIKE_POST: 
       return posts.map((post) => post._id === action.payload._id? action.payload: post);
-    case 'DELETE_POST':
+    case DELETE_POST:
       return posts.filter(post => post._id !== action.payload);
-    case 'UPDATE_POST':
+    case UPDATE_POST:
       return posts.map((post) => post._id === action.payload._id? action.payload : post);
-    case 'FETCH_ALL_POSTS':
+    case FETCH_ALL_POSTS:
       return action.payload;
-    case 'CREATE_POST':
+    case CREATE_POST:
       return [...posts, action.payload];
     default:
       return posts;    
