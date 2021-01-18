56c540ec11f99e863ac634b0358351d3b87a0a2f
kiran-code <ravikiransjce.code@gmail.com>
2021-01-18 08:19:33 +0530

    Implemented POSTs UI and get works automatically

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import Post from './Post/Post';
+import React from "react";
+import { useSelector } from "react-redux";
+import Post from "./Post/Post";
 
-import { Grid, CircularProgress } from '@material-ui/core';
+import { Grid, CircularProgress } from "@material-ui/core";
 
-import useStyles from './styles';
+import useStyles from "./styles";
 
 const Posts = () => {
   const posts = useSelector((state) => {
@@ -13,19 +13,22 @@ const Posts = () => {
   const classes = useStyles();
   console.log(posts);
 
-  return (
-   !posts.length ? <CircularProgress/> : (
-     <Grid className={classes.container} container alignItems="stretch" spacing={3}>
-       {
-         posts.map(post => (
-           <Grid key={post._id} item xs={12} sm={6}>
-             <Post post={post}/>
-           </Grid>
-         ))
-       }
-     </Grid>
-   )
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.container}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
+      {posts.map((post) => (
+        <Grid key={post._id} item xs={12} sm={6}>
+          <Post post={post} />
+        </Grid>
+      ))}
+    </Grid>
   );
-}
+};
 
 export default Posts;
